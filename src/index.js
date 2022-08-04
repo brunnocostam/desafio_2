ae4c169d9ccc6582dec78766f371cf9df627c774
Brunno Costa <brunnocostam@gmail.com>
2022-08-04 01:29:33 -0300

    almost all tests passed, just details to be fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 
-const { v4: uuidv4, validate } = require('uuid');
+const { v4: uuidv4, validate, v4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
@@ -10,19 +10,71 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404);
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if ( user.todos.length == 10 && user.pro == false ) {
+    return response.status(403);
+  }
+
+  return next ();
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  
+  if (!user) {
+    return response.status(404);
+  }
+  if (!validate(id)) {
+    return response.status(400)
+  }
+  
+  const todo = user.todos.find((todo) => todo.id === id);
+  
+
+  if(!todo || todo == undefined) {
+    return response.status(404);
+  }
+  
+
+  request.user = user;
+  request.todo = todo;
+  
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).send;
+  }
+
+  request.user = user;
+
+  next();
+
 }
 
 app.post('/users', (request, response) => {
